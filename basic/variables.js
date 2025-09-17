6a91ed8bdb2e7fc1742da3e41e7ec3aa976928ab
Tanishqji <104748593+Tanishqji@users.noreply.github.com>
2025-09-17 15:18:08 +0000

    confid done

---
+++ basic/variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+ let accountEmail = "tanishq123@gmail.com"
+ var accountPassword = "12345"
+ accountCity = "indore"
+
+ // accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "noida"
+
+console.table([accountId ,accountEmail,accountPassword,accountCity])
