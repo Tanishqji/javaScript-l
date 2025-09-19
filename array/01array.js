054a6e9e9cb338b8c21dd3df3e9754427236f9a2
Tanishqji <104748593+Tanishqji@users.noreply.github.com>
2025-09-19 10:17:18 +0000

    array 1

---
+++ array/01array.js
@@ -0,0 +1,34 @@
+const myArr = [0,1,2,3,4,5]
+//console.log(1);
+
+//array methods
+
+//myArr.push(8)
+//myArr.pop()
+
+//myArr.unshift(9)
+//myArr.shift()
+
+//console.log(myArr.includes(4));
+//console.log(myArr.indexOf(4));
+
+//const newArr = myArr.join()
+//console.log(newArr);
+
+console.log("A",myArr);
+const myn1 = myArr.slice(1,4)
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,4)
+console.log(myn2);
+console.log("C",myArr);
+
+
+
+
+
+
+
+
+
