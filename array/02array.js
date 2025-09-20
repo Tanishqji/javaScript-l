93022d6c6755ae8f2404742f02a6f3e99016ee35
Tanishqji <104748593+Tanishqji@users.noreply.github.com>
2025-09-20 06:39:34 +0000

    array 2

---
+++ array/02array.js
@@ -0,0 +1,20 @@
+const myhero1 = ["spiderman", "ironman", "hulk"];
+const myhero2 = ["arjun", "hanuman", "ram"];
+
+//const allhero = [...myhero1, ...myhero2];
+//console.log(allhero);
+ 
+//myhero1.push(myhero2)
+//console.log(myhero1);
+
+//const allhero= myhero1.concat(myhero2)
+//console.log(allhero);
+
+const arr1 = [1, 2, 3, [4, 5, 6], 7, [8, 9, [10, 11]]];
+const arr2 = arr1.flat(Infinity);
+console.log(arr2);
+
+console.log(Array.from("tanishq"));
+
+
+
